7d5582e3a85a04bbb749c0d0e89fe5a961201fbf
Obed-Padilla <128347280+Obed-Padilla@users.noreply.github.com>
2023-03-19 23:20:00 -0600

    Add files via upload

---
+++ Sorting.java
@@ -0,0 +1,411 @@
+public class Sorting {
+
+    public Sorting () {
+
+    }
+
+    public static void print(String[] arr) {
+        for (int i = 0; i < arr.length; ++i)
+        {
+            for (int index = 0; index < 5; ++index)
+            {
+                System.out.print(arr[i] + "\t");
+            }
+            System.out.println();
+        }
+        
+    }
+
+    // Bubble Sort Algorithm.
+    public static void bubbleSort(String[] arr) {
+        bubbleSort(arr, arr.length);
+    }
+
+    public static void reverseBubbleSort(String[] arr) {
+        reverseBubbleSort(arr, arr.length);
+    }
+
+    private static void bubbleSort(String[] arr, int n) {
+
+        for (int i = 0; i < n - 1; ++i)
+        {
+            for (int j = 0; j < n - i - 1; ++j)
+            {
+                if (arr[j + 1].compareTo(arr[j]) < 0)
+                {
+                    String temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+    private static void reverseBubbleSort(String[] arr, int n) {
+
+        for (int i = 0; i < n - 1; ++i)
+        {
+            for (int j = 0; j < n - i - 1; ++j)
+            {
+                if (arr[j + 1].compareTo(arr[j]) > 0)
+                {
+                    String temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+
+    // Selection Sort.
+    public static void selectionSort(String[] arr) {
+        selectionSort(arr, arr.length);
+    }
+
+    public static void reverseSelectionSort(String[] arr) {
+        reverseSelectionSort(arr, arr.length);
+    }
+
+    private static void selectionSort(String[] arr, int n) {
+
+        for (int i = 0; i < n - 1; ++i)
+        {
+            int small = i;
+            for (int j = i + 1; j < n; ++j)
+            {
+                if (arr[j].compareTo(arr[small]) < 0)
+                {
+                    small = j;
+                }
+            }
+
+            String temp = arr[i];
+            arr[i] = arr[small];
+            arr[small] = temp;
+        }
+    }
+
+    private static void reverseSelectionSort(String[] arr, int n) {
+
+        for (int i = 0; i < n - 1; ++i)
+        {
+            int small = i;
+            for (int j = i + 1; j < n; ++j)
+            {
+                if (arr[j].compareTo(arr[small]) > 0)
+                {
+                    small = j;
+                }
+            }
+
+            String temp = arr[i];
+            arr[i] = arr[small];
+            arr[small] = temp;
+        }
+    }
+
+    // Insertion Sort.
+    public static void insertionSort(String[] arr) {
+        insertionSort(arr, arr.length);
+    }
+
+    public static void reverseInsertionSort(String[] arr) {
+        reverseInsertionSort(arr, arr.length);
+    }
+
+    private static void insertionSort(String[] arr, int n) {
+
+        for (int i = 1; i < n; ++i)
+        {
+            String temp = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j].compareTo(temp) > 0)
+            {
+                arr[j + 1] = arr[j];
+                --j;
+            }
+            arr[j + 1] = temp;
+        }
+    }
+
+    private static void reverseInsertionSort(String[] arr, int n) {
+
+        for (int i = 1; i < n; ++i)
+        {
+            String temp = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j].compareTo(temp) < 0)
+            {
+                arr[j + 1] = arr[j];
+                --j;
+            }
+            arr[j + 1] = temp;
+        }
+    }
+
+    // Quick Sort Algorithm.
+    public static void quickSort(String[] arr) {
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    public static void reverseQuickSort(String[] arr) {
+        reverseQuickSort(arr, 0, arr.length - 1);
+    }
+
+    private static void quickSort(String[] arr, int start, int end) {
+        if (end <= start) return;
+
+        int pivot = partition(arr, start, end);
+        quickSort(arr, start, pivot);
+        quickSort(arr, pivot + 1, end);
+    }
+
+    private static int partition(String[] arr, int start, int end) {
+        String pivot = arr[start + (end - start) / 2];
+        int i = start-1;
+        int j = end+1;
+
+        while(true)
+        {
+            while (arr[++i].compareTo(pivot) < 0);
+            while (arr[--j].compareTo(pivot) > 0);
+
+            if(i >= j)
+                return j;
+
+            String temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    private static void reverseQuickSort(String[] arr, int start, int end) {
+        if (end <= start) return;
+
+        int pivot = partitionReverse(arr, start, end);
+        reverseQuickSort(arr, start, pivot);
+        reverseQuickSort(arr, pivot + 1, end);
+    }
+
+    private static int partitionReverse(String[] arr, int start, int end) {
+        String pivot = arr[start + (end - start) / 2];
+        int i = start - 1;
+        int j = end + 1;
+
+        while(true)
+        {
+            while (arr[++i].compareTo(pivot) > 0);
+            while (arr[--j].compareTo(pivot) < 0);
+
+            if(i >= j)
+                return j;
+
+            String temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    // Merge Sort Algorithm
+    public static void mergeSort(String[] arr) {
+        mergeSort(arr, 0, arr.length - 1);
+    }
+
+    public static void reverseMergeSort(String[] arr) {
+        reverseMergeSort(arr, 0, arr.length - 1);
+    }
+
+    private static void mergeSort(String[] arr, int left, int right) {
+    
+        if (right <= left) return; // base case
+        
+        int middle = left + (right - left) / 2;
+        mergeSort(arr, left, middle);
+        mergeSort(arr, middle + 1, right);
+        merge(arr, left, middle, right);
+    }
+    
+    private static void merge(String[] arr, int left, int middle, int right) {
+        String[] temp = new String[right - left + 1];
+    
+        int i = left, j = middle + 1, k = 0;
+    
+        while (i <= middle && j <= right)
+        {
+            if (arr[i].compareTo(arr[j]) < 0)
+            {
+                temp[k] = arr[i];
+                ++i;
+            } 
+            else
+            {
+                temp[k] = arr[j];
+                ++j;
+            }
+    
+            ++k;
+        }
+    
+        while (i <= middle)
+        {
+            temp[k] = arr[i];
+            ++i;
+            ++k;
+        }
+    
+        while (j <= right)
+        {
+            temp[k] = arr[j];
+            ++j;
+            ++k;
+        }
+    
+        for (int l = 0; l < temp.length; ++l)
+        {
+            arr[left + l] = temp[l];
+        }
+    }
+
+    private static void reverseMergeSort(String[] arr, int left, int right) {
+    
+        if (right <= left) return; // base case
+        
+        int middle = left + (right - left) / 2;
+        reverseMergeSort(arr, left, middle);
+        reverseMergeSort(arr, middle + 1, right);
+        mergeReverse(arr, left, middle, right);
+    }
+    
+    private static void mergeReverse(String[] arr, int left, int middle, int right) {
+        String[] temp = new String[right - left + 1];
+    
+        int i = left, j = middle + 1, k = 0;
+    
+        while (i <= middle && j <= right)
+        {
+            if (arr[i].compareTo(arr[j]) > 0)
+            {
+                temp[k] = arr[i];
+                ++i;
+            } 
+            else
+            {
+                temp[k] = arr[j];
+                ++j;
+            }
+    
+            ++k;
+        }
+    
+        while (i <= middle)
+        {
+            temp[k] = arr[i];
+            ++i;
+            ++k;
+        }
+    
+        while (j <= right)
+        {
+            temp[k] = arr[j];
+            ++j;
+            ++k;
+        }
+    
+        for (int l = 0; l < temp.length; ++l)
+        {
+            arr[left + l] = temp[l];
+        }
+    }
+
+    // Pancake Sort
+    public static void pancakeSort(String[] arr) {
+        pancakeSort(arr, arr.length - 1);
+    }
+
+    public static void reversePancakeSort(String[] arr) {
+        reversePancakeSort(arr, arr.length - 1);
+    }
+
+    private static void pancakeSort(String[] arr, int i) {
+
+        for (; i > 0; --i)
+        {
+            int maxIndex = getMaxIndex(arr, i);
+
+            if (maxIndex != i) // avoids unnecessary method calls by fliping twice from the same starting and ending positions...
+            {
+                flip(arr, maxIndex); // flips elements from index 0 to the index of the max element
+                flip(arr, i); // moves element at 0 (where max element is now) to the very last element before i updates and flips everything else in its path
+            }
+        }
+    }
+    
+    private static int getMaxIndex(String[] arr, int right) {
+        int maxIndex = 0;
+
+        // finds biggest value at an index
+        for (int i = 1; i <= right; ++i)
+        {
+            if (arr[i].compareTo(arr[maxIndex]) > 0)
+                maxIndex = i;
+        }
+
+        return maxIndex;
+    }
+    
+    private static void flip(String[] arr, int endToStart) {
+        int startToEnd = 0;
+
+        // reverses array from index 0 to the end point provided...
+        while (startToEnd < endToStart)
+        {
+            String temp = arr[startToEnd];
+            arr[startToEnd] = arr[endToStart];
+            arr[endToStart] = temp;
+
+            ++startToEnd; --endToStart; // updates variables to keep reversing...
+        }
+    }
+
+    private static void reversePancakeSort(String[] arr, int i) {
+
+        for (; i > 0; --i)
+        {
+            int minIndex = getMinIndexInReverse(arr, i);
+
+            if (minIndex != i) // avoids unnecessary method calls by fliping twice from the same starting and ending positions...
+            {
+                flipReverse(arr, minIndex); // flips elements from index 0 to the index of the min element
+                flipReverse(arr, i); // moves element at 0 (where min element is now) to the very last element before i updates and flips everything else in its path
+            }
+        }
+    }
+    
+    private static int getMinIndexInReverse(String[] arr, int right) {
+        int minIndex = 0;
+
+        // finds smallest value at an index
+        for (int i = 1; i <= right; ++i)
+        {
+            if (arr[i].compareTo(arr[minIndex]) < 0)
+                minIndex = i;
+        }
+
+        return minIndex;
+    }
+    
+    private static void flipReverse(String[] arr, int endToStart) {
+        int startToEnd = 0;
+
+        // reverses array from index 0 to the end point provided...
+        while (startToEnd < endToStart)
+        {
+            String temp = arr[startToEnd];
+            arr[startToEnd] = arr[endToStart];
+            arr[endToStart] = temp;
+
+            ++startToEnd; --endToStart; // updates variables to keep reversing...
+        }
+    }
+
+
+}
---
+++ Tester.java
@@ -0,0 +1,202 @@
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.Locale;
+import java.text.NumberFormat;
+
+/*
+ * tests algorithms performance in nanoseconds to sort an array of Strings (values are derived from the file "dictionary.txt")
+ * 
+ * variables that calculate their time are numbered as follows:
+ *  - by number of algorithm (algorithms numbered as bubble sort being 1, selection sort being 2....)
+ *  - and by number of test case (unsorted array being 1, already sorted being 2, and sorted to reversed being 3)
+ * 
+ * Example: timeLapsed11 means Bubble Sort is being tested with an unsorted array...
+ */
+public class Tester {
+    public static void main(String[] args) throws IOException {
+        NumberFormat formatter = NumberFormat.getNumberInstance(Locale.US); // formats the amount of nanoseconds displayed
+
+        List<String> words = Files.readAllLines(Paths.get("15000Words.txt"), StandardCharsets.UTF_8); // reads txt file and stores it in an array list
+  
+        String[] transformed = words.toArray(new String[words.size()]); // transforms array list into a String array...
+        String[] wordsToSort = transformed.clone(); // makes a copy to safely change it in the future...
+
+        // Warm-up Phase (triggers the JVM optimizations)......
+        for (int i = 0; i < 550; i++)
+        {
+                List<String> getSmaller = Files.readAllLines(Paths.get("2500Words.txt"), StandardCharsets.UTF_8);
+                String[] warmupArray = getSmaller.toArray(new String[getSmaller.size()]);
+                Sorting.bubbleSort(warmupArray);
+                warmupArray = getSmaller.toArray(new String[getSmaller.size()]);
+                Sorting.selectionSort(warmupArray);
+                warmupArray = getSmaller.toArray(new String[getSmaller.size()]);
+                Sorting.insertionSort(warmupArray);
+                warmupArray = getSmaller.toArray(new String[getSmaller.size()]);
+                Sorting.quickSort(warmupArray);
+                warmupArray = getSmaller.toArray(new String[getSmaller.size()]);
+                Sorting.mergeSort(warmupArray);
+                warmupArray = getSmaller.toArray(new String[getSmaller.size()]);
+                Sorting.pancakeSort(warmupArray);
+        }
+// ---------------------------------------------------------------------------------------------------------
+
+
+
+
+        // Testing Sorting algorithms......
+// ----------------- (1) Bubble Sort -----------------------------
+        // Unsorted
+        long startTime11 = System.nanoTime();
+        Sorting.bubbleSort(wordsToSort);
+        long endTime11 = System.nanoTime();
+        long timeLapsed11 = endTime11 - startTime11;
+        // Already sorted
+        long startTime12 = System.nanoTime();
+        Sorting.bubbleSort(wordsToSort);
+        long endTime12 = System.nanoTime();
+        long timeLapsed12 = endTime12 - startTime12;
+        // Sorted to reversed
+        long startTime13 = System.nanoTime();
+        Sorting.reverseBubbleSort(wordsToSort);
+        long endTime13 = System.nanoTime();
+        long timeLapsed13 = endTime13 - startTime13;
+        // formats time with commas
+        String formated11 = formatter.format(timeLapsed11); // Time taken for Bubble Sort formated (unsorted values)
+        String formated12 = formatter.format(timeLapsed12); // Time taken for Bubble Sort formated (already sorted values)
+        String formated13 = formatter.format(timeLapsed13); // Time taken for Bubble Sort formated (sorted to reversed values)
+// ----------------- (2) Selection Sort -----------------------------
+        // Unsorted
+        wordsToSort = transformed.clone();
+        long startTime21 = System.nanoTime();
+        Sorting.selectionSort(wordsToSort);
+        long endTime21 = System.nanoTime();
+        long timeLapsed21 = endTime21 - startTime21;
+        // Already Sorted
+        long startTime22 = System.nanoTime();
+        Sorting.selectionSort(wordsToSort);
+        long endTime22 = System.nanoTime();
+        long timeLapsed22 = endTime22 - startTime22;
+        // Sorted to reversed
+        long startTime23 = System.nanoTime();
+        Sorting.reverseSelectionSort(wordsToSort);
+        long endTime23 = System.nanoTime();
+        long timeLapsed23 = endTime23 - startTime23;
+        // formats time with commas
+        String formated21 = formatter.format(timeLapsed21); // Time taken for Selection Sort formated (unsorted values)
+        String formated22 = formatter.format(timeLapsed22); // Time taken for Selection Sort formated (already sorted values)
+        String formated23 = formatter.format(timeLapsed23); // Time taken for Selection Sort formated (sorted to reversed values)
+// ----------------- (3) Insertion Sort -----------------------------
+        // Unsorted
+        wordsToSort = transformed.clone();
+        long startTime31 = System.nanoTime();
+        Sorting.insertionSort(wordsToSort);
+        long endTime31 = System.nanoTime();
+        long timeLapsed31 = endTime31 - startTime31;
+        // Already Sorted
+        long startTime32 = System.nanoTime();
+        Sorting.insertionSort(wordsToSort);
+        long endTime32 = System.nanoTime();
+        long timeLapsed32 = endTime32 - startTime32;
+        // Sorted to reversed
+        long startTime33 = System.nanoTime();
+        Sorting.reverseInsertionSort(wordsToSort);
+        long endTime33 = System.nanoTime();
+        long timeLapsed33 = endTime33 - startTime33;
+        // formats time with commas
+        String formated31 = formatter.format(timeLapsed31); // Time taken for Insertion Sort formated (unsorted values)
+        String formated32 = formatter.format(timeLapsed32); // Time taken for Insertion Sort formated (already sorted values)
+        String formated33 = formatter.format(timeLapsed33); // Time taken for Insertion Sort formated (sorted to reversed values)
+// ----------------- (4) Quick Sort -----------------------------
+        // Unsorted
+        wordsToSort = transformed.clone();
+        long startTime41 = System.nanoTime();
+        Sorting.quickSort(wordsToSort);
+        long endTime41 = System.nanoTime();
+        long timeLapsed41 = endTime41 - startTime41;
+        // Already sorted
+        long startTime42 = System.nanoTime();
+        Sorting.quickSort(wordsToSort);
+        long endTime42 = System.nanoTime();
+        long timeLapsed42 = endTime42 - startTime42;
+        // Sorted to reversed
+        long startTime43 = System.nanoTime();
+        Sorting.reverseQuickSort(wordsToSort);
+        long endTime43 = System.nanoTime();
+        long timeLapsed43 = endTime43 - startTime43;
+        // formats time with commas
+        String formated41 = formatter.format(timeLapsed41); // Time taken for Quick Sort formated (unsorted values)
+        String formated42 = formatter.format(timeLapsed42); // Time taken for Quick Sort formated (already sorted values)
+        String formated43 = formatter.format(timeLapsed43); // Time taken for Quick Sort formated (sorted to reversed values)
+// ----------------- (5) Merge Sort -----------------------------
+        // Unsorted
+        wordsToSort = transformed.clone();
+        long startTime51 = System.nanoTime();
+        Sorting.mergeSort(wordsToSort);
+        long endTime51 = System.nanoTime();
+        long timeLapsed51 = endTime51 - startTime51;
+        // Already sorted
+        long startTime52 = System.nanoTime();
+        Sorting.mergeSort(wordsToSort);
+        long endTime52 = System.nanoTime();
+        long timeLapsed52 = endTime52 - startTime52;
+        // Sorted to reversed
+        long startTime53 = System.nanoTime();
+        Sorting.reverseMergeSort(wordsToSort);
+        long endTime53 = System.nanoTime();
+        long timeLapsed53 = endTime53 - startTime53;
+        // formats time with commas
+        String formated51 = formatter.format(timeLapsed51); // Time taken for Merge Sort formated (unsorted values)
+        String formated52 = formatter.format(timeLapsed52); // Time taken for Merge Sort formated (already sorted values)
+        String formated53 = formatter.format(timeLapsed53); // Time taken for Merge Sort formated (sorted to reversed values)
+// ----------------- (6) Pancake Sort -----------------------------
+        // Unsorted
+        wordsToSort = transformed.clone();
+        long startTime61 = System.nanoTime();
+        Sorting.pancakeSort(wordsToSort);
+        long endTime61 = System.nanoTime();
+        long timeLapsed61 = endTime61 - startTime61;
+        // Already sorted
+        long startTime62 = System.nanoTime();
+        Sorting.pancakeSort(wordsToSort);
+        long endTime62 = System.nanoTime();
+        long timeLapsed62 = endTime62 - startTime62;
+        // Sorted to reversed
+        long startTime63 = System.nanoTime();
+        Sorting.reversePancakeSort(wordsToSort);
+        long endTime63 = System.nanoTime();
+        long timeLapsed63 = endTime63 - startTime63;
+        // formats time with commas
+        String formated61 = formatter.format(timeLapsed61); // Time taken for Pancake Sort formated (unsorted values)
+        String formated62 = formatter.format(timeLapsed62); // Time taken for Pancake Sort formated (already sorted values)
+        String formated63 = formatter.format(timeLapsed63); // Time taken for Pancake Sort formated (sorted to reversed values)
+
+        System.out.println("========================================= Results ==========================================");
+        System.out.println("Time taken by the Bubble Sort algorithm: \n\tUnsorted values: " + formated11 + " nanoseconds.");
+        System.out.println("\tSorted values: " + formated12 + " nanoseconds.");
+        System.out.println("\tSorted to reversed: " + formated13 + " nanoseconds.");
+        System.out.println("---------------------------------------------------------------------------------------------");
+        System.out.println("Time taken by the Selection Sort algorithm: \n\tUnsorted values: " + formated21 + " nanoseconds.");
+        System.out.println("\tSorted values: " + formated22 + " nanoseconds.");
+        System.out.println("\tSorted to reversed: " + formated23 + " nanoseconds.");
+        System.out.println("---------------------------------------------------------------------------------------------");
+        System.out.println("Time taken by the Insertion Sort algorithm: \n\tUnsorted values: " + formated31 + " nanoseconds.");
+        System.out.println("\tSorted values: " + formated32 + " nanoseconds.");
+        System.out.println("\tSorted to reversed: " + formated33 + " nanoseconds.");
+        System.out.println("---------------------------------------------------------------------------------------------");
+        System.out.println("Time taken by the Quick Sort algorithm: \n\tUnsorted values: " + formated41 + " nanoseconds.");
+        System.out.println("\tSorted values: " + formated42 + " nanoseconds.");
+        System.out.println("\tSorted to reversed: " + formated43 + " nanoseconds.");
+        System.out.println("---------------------------------------------------------------------------------------------");
+        System.out.println("Time taken by the Merge Sort algorithm: \n\tUnsorted values: " + formated51 + " nanoseconds.");
+        System.out.println("\tSorted values: " + formated52 + " nanoseconds.");
+        System.out.println("\tSorted to reversed: " + formated53 + " nanoseconds.");
+        System.out.println("---------------------------------------------------------------------------------------------");
+        System.out.println("Time taken by the Pancake Sort algorithm: \n\tUnsorted values: " + formated61 + " nanoseconds.");
+        System.out.println("\tSorted values: " + formated62 + " nanoseconds.");
+        System.out.println("\tSorted to reversed: " + formated63 + " nanoseconds.");
+        System.out.println("============================================================================================");
+    }
+}
